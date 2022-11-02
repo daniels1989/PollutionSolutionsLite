beb8919ac10570aa31b61114d7fc00c888ca593e
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2022-11-02 19:36:10 +0100

    remove entity resistances

--- data-updates.lua
+++ data-updates.lua
@@ -41,43 +41,12 @@ local function addResistance(entityList, _DamageType, _Percent, _Decrease)
     else
       table.insert(entityList[name].resistances, resistTable)
     end
-  end
+ end
 end
 
 -----------------
 -- Resistances --
 -----------------
-
-addResistance(data.raw["wall"],					POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["gate"],					POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["transport-belt"],		POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["car"],					POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["electric-turret"],		POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["ammo-turret"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["construction-robot"],	POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["combat-robot"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["logistic-robot"],		POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["logistic-container"],	POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["container"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["electric-pole"],		POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["heat-pipe"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["pipe"],					POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["pipe-to-ground"],		POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["land-mine"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["straight-rail"],		POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["curved-rail"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["train-stop"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["locomotive"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["rail-signal"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["rail-chain-signal"],	POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["underground-belt"],		POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["lamp"],					POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["cargo-wagon"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["fluid-wagon"],			POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["artillery-wagon"],		POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["tree"],					POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["radar"],				POLLUTION_DAMAGE_TYPE, 100)
-addResistance(data.raw["unit-spawner"],			POLLUTION_DAMAGE_TYPE, 100)
 
 addResistance({data.raw["armor"]["modular-armor"]},		POLLUTION_DAMAGE_TYPE, 10, 5)
 addResistance({data.raw["armor"]["power-armor"]},		POLLUTION_DAMAGE_TYPE, 30, 10)
