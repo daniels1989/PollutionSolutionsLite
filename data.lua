66bfe74514168e423eaafb429e13b82af8401e9e
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2023-02-18 16:29:29 +0100

    Add k2 flare stack compatibility

--- data.lua
+++ data.lua
@@ -8,3 +8,8 @@ require("prototypes.technology")
 
 require("prototypes.pollutioncollector")
 require("prototypes.hevsuit")
+
+if mods["k2-flare-stack"] then
+	flare_stack.flare_stack_util.addBurnFluidEmissionsMultiplier("polluted-air", 600)
+	flare_stack.flare_stack_util.addBurnFluidEmissionsMultiplier("toxic-sludge", 600 * AIR_PER_SLUDGE)
+end
