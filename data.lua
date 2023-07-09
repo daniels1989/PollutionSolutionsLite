7f73773f4e15e5713b9b88537aeeb87140c59136
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2023-07-09 16:22:31 +0200

    Add categories in again

--- data.lua
+++ data.lua
@@ -1,3 +1,4 @@
 require("constants")
 
+require("prototypes.category")
 require("prototypes.fluid")
---
+++ prototypes/category.lua
@@ -0,0 +1,10 @@
+data:extend({
+  {
+    type = "damage-type",
+    name = "toxic",
+  },
+  {
+    type = "recipe-category",
+    name = "pollution"
+  },
+})
