45f4a988c30b88090fb08a929473c1da3e2eec2a
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2024-12-29 19:47:36 +0100

    Add airborne pollution property to planets
    make sure pollution and spores are always 1 and 2

--- prototypes/planet/add-airborne-pollutant.lua
+++ prototypes/planet/add-airborne-pollutant.lua
@@ -1,7 +1,20 @@
+-- Require utils
+SharedUtils = require 'shared/util'
+
 pollutants = {
-  nil,
+  'pollution',
+  'spores'
 }
 
 for _, pollutant in pairs(data.raw['airborne-pollutant']) do
-  table.insert(pollutants, pollutant.name)
+  if not SharedUtils.table.contains(pollutants, pollutant.name) then
+    table.insert(pollutants, pollutant.name)
+  end
+end
+
+for _, planet in pairs(data.raw['planet']) do
+  if planet.pollutant_type ~= nil and SharedUtils.table.contains(pollutants, planet.pollutant_type) then
+    local type = SharedUtils.table.search(pollutants, planet.pollutant_type)
+    data.raw['planet'][planet.name].surface_properties['airborne-pollutant'] = type or 0
+  end
 end
