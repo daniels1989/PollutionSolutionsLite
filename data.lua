370f6896d1ef7c6732f5079ac04e7f6d72a2182f
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2025-03-23 21:46:13 +0100

    Early test with mining drill

--- data.lua
+++ data.lua
@@ -3,3 +3,14 @@ require "constants"
 require "prototypes/category"
 require "prototypes/fluid"
 require "prototypes/surface-property"
+
+local test_resource = util.table.deepcopy(data.raw['resource']['crude-oil'])
+test_resource.name = "test-pollution"
+test_resource.flags = {"placeable-neutral", "placeable-off-grid", "not-on-map"}
+test_resource.collision_box = {{-0.1, -0.1}, {0.1, 0.1}}
+data:extend({test_resource})
+
+local test_jack = util.table.deepcopy(data.raw['mining-drill']['pumpjack'])
+test_jack.name = "test-pollution-jack"
+test_jack.resource_searching_radius = 2.49
+data:extend({test_jack})
